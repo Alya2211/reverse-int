eb84b01004c2538c182ff7cd25869286bd7c69c9
Alya2211 <azinovey@mail.ru>
2023-10-29 00:21:56 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+	return myReverse(n);
+}
+
+function myReverse(n) {
+	if (n < 0) {
+		n = 0 - n;
+	}
+
+	return recurseReverse(n)[0];
+}
+
+function recurseReverse(number, degree = 0) {
+	if (number === 0) {
+		return [number, 0];
+	};
+	var previousNumberAndDegree = recurseReverse(
+		(number - number % 10) / 10,
+	);
+	var result = (number % 10 * 10 ** previousNumberAndDegree[1]) + previousNumberAndDegree[0]
+	return [result, (previousNumberAndDegree[1] + 1)];
 }
